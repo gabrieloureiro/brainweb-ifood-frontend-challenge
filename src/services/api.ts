82dc10ff0eb1d20dded541b88b64f4e83307326b
Gabriel Loureiro <gabrieloureirof@gmail.com>
2021-02-26 01:44:21 -0300

    update .env to request api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333/'
+      : process.env.NEXT_PUBLIC_IFOOD_API,
   responseType: 'json',
   headers: {
     'content-type': 'application/json',
